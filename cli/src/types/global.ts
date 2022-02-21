1547ad8ab3a4ec2b2f921bd0b81960f5eea9face
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-02-21 17:24:32 -0300

    Criado a tipagem dos dados vindos do json

---
+++ cli/src/types/global.ts
@@ -0,0 +1,11 @@
+export type DescriptionData = {
+    informacoes_principais: string[];
+    imagens: {
+        imagem_desktop: string;
+        imagem_mobile: string;
+    };
+    beneficios: string;
+    composicao: string;
+    modo_de_usar: string;
+    advertencias: string;
+}
