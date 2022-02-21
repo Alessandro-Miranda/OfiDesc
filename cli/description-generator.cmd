ddc37fc306404c8c7f90e46e85745bbeabf9d68b
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-02-21 17:26:46 -0300

    Atualizado o script para inicializar o arquivo node

--- cli/description-generator.cmd
+++ cli/description-generator.cmd
@@ -1,8 +1,12 @@
 @echo off
 
-for %%I in (.) do set CurrDirName=%~dpnx0
+@REM for %%I in (.) do set CurrDirName=%~dpnx0 // Obtém com o nome do arquivo
+
+@REM Obtém o path de onde o arquivo está sendo executado
+for %%I in (.) do set CurrDirName=%~dp0
+
 set Script=%USERPROFILE%\description-generator\dist\index.js
 
-node %Script% --%CurrDirName% --%USERPROFILE%
+node %Script% --dirname=%CurrDirName% --userProfile=%USERPROFILE% --entry=%CurrDirName%skus
 
 pause
