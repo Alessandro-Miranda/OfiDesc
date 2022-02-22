ef52717598b388ac83dd902ad9d6eac3c390a657
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-02-22 11:41:56 -0300

    Atualizado a inicialização da geração das descrições

--- cli/description-generator.cmd
+++ cli/description-generator.cmd
@@ -1,12 +1,13 @@
 @echo off
 
-@REM for %%I in (.) do set CurrDirName=%~dpnx0 // Obtém com o nome do arquivo
+for %%I in (.) do set CurrDirName=%~dpnx0 // Obtém com o nome do arquivo
 
-@REM Obtém o path de onde o arquivo está sendo executado
+@REM Obtém o path de onde o arquivo está sendo executado com barra no final
 for %%I in (.) do set CurrDirName=%~dp0
 
-set Script=%USERPROFILE%\description-generator\dist\index.js
+@REM set Script=%USERPROFILE%\description-generator\dist\index.js
 
-node %Script% --dirname=%CurrDirName% --userProfile=%USERPROFILE% --entry=%CurrDirName%skus
+set Script=%CurrDirName%dist\index.js
+set NODE_ENV=prod && node %Script% --dirname=%CurrDirName% --userProfile=%USERPROFILE% --entry=%CurrDirName%skus --output=%CurrDirName%codigo-gerado
 
 pause
