a813f82d501e7bd3590a069fa233440634b1b438
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-02-25 14:21:35 -0300

    corrigido o path do script

--- cli/description-generator.cmd
+++ cli/description-generator.cmd
@@ -7,7 +7,7 @@ for %%I in (.) do set CurrDirName=%~dp0
 
 @REM set Script=%USERPROFILE%\description-generator\dist\index.js
 
-set Script=%CurrDirName%\index.js
+set Script=%CurrDirName%index.js
 set NODE_ENV=prod && node %Script% --dirname=%CurrDirName% --userProfile=%USERPROFILE% --entry=%CurrDirName%skus --output=codigo-gerado
 
 pause
