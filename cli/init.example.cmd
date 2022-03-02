2622c4143469e32cd4b3acf0582dbacb67a5e3d5
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-03-02 14:00:32 -0300

    Atualizado o exemplo

--- cli/init.example.cmd
+++ cli/init.example.cmd
@@ -22,26 +22,26 @@ mkdir %BaseDir%\interfaces
 
 echo Baixando arquivos necessarios...
 
-cd %BaseDir% && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/index.js --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/index.d.ts --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/description.js --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/description.d.ts --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/description-generator.cmd --ssl-no-revoke
+cd %BaseDir% && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke
 
-attrib +h +r +a /s /d  %BaseDir%\index.js
-attrib +h +r +a /s /d  %BaseDir%\index.d.ts
-attrib +h +r +a /s /d  %BaseDir%\description.js
-attrib +h +r +a /s /d  %BaseDir%\description.d.ts
+attrib +h +r +a /s /d %BaseDir%\index.js
+attrib +h +r +a /s /d %BaseDir%\index.d.ts
+attrib +h +r +a /s /d %BaseDir%\description.js
+attrib +h +r +a /s /d %BaseDir%\description.d.ts
 
-cd utils && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/utils/generateHTML.d.ts --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/utils/generateHTML.js --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/utils/minifyHTML.d.ts --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/utils/minifyHTML.js --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/utils/optionsChoiceMenu.d.ts --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/utils/optionsChoiceMenu.js --ssl-no-revoke && cd ..
+cd utils && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && cd ..
 
 attrib +h +r +a /s /d %BaseDir%\utils
 
-cd types && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/types/global.d.ts --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/types/global.js --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/types/menu.d.ts --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/types/menu.js --ssl-no-revoke && cd ..
+cd types && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && cd ..
 
 attrib +h +r +a /s /d  %BaseDir%\types
 
-cd interfaces && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/interfaces/IDescription.d.ts --ssl-no-revoke && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/interfaces/IDescription.js --ssl-no-revoke && cd ..
+cd interfaces && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && cd ..
 
-attrib +h +r +a /s /d  %BaseDir%\interfaces
+attrib +h +r +a /s /d %BaseDir%\interfaces
 
-cd exemplo-formato-arquivo && curl -O https://arquivos.oficialfarma.com.br/dev/gerador-descricao/arquivo-json-exemplo.json --ssl-no-revoke && cd ..
+cd exemplo-formato-arquivo && curl -O {URL} --ssl-no-revoke && cd ..
 
 echo Verificando a instalacao do Node.js
 
