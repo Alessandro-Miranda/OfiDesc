d4b214d0b6d83e77d8f9524f6b1734f183a3dd39
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-03-07 08:54:54 -0300

    Atualizado o exemplo

--- cli/init.example.cmd
+++ cli/init.example.cmd
@@ -22,26 +22,26 @@ mkdir %BaseDir%\interfaces
 
 echo Baixando arquivos necessarios...
 
-cd %BaseDir% && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke
+cd %BaseDir% && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke
 
 attrib +h +r +a /s /d %BaseDir%\index.js
 attrib +h +r +a /s /d %BaseDir%\index.d.ts
 attrib +h +r +a /s /d %BaseDir%\description.js
 attrib +h +r +a /s /d %BaseDir%\description.d.ts
 
-cd utils && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && cd ..
+cd utils && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && cd ..
 
 attrib +h +r +a /s /d %BaseDir%\utils
 
-cd types && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && cd ..
+cd types && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && cd ..
 
 attrib +h +r +a /s /d  %BaseDir%\types
 
-cd interfaces && curl -O {URL} --ssl-no-revoke && curl -O {URL} --ssl-no-revoke && cd ..
+cd interfaces && curl -m 600 -O {URL} --ssl-no-revoke && curl -m 600 -O {URL} --ssl-no-revoke && cd ..
 
 attrib +h +r +a /s /d %BaseDir%\interfaces
 
-cd exemplo-formato-arquivo && curl -O {URL} --ssl-no-revoke && cd ..
+cd exemplo-formato-arquivo && curl -m 600 -O {URL} --ssl-no-revoke && cd ..
 
 echo Verificando a instalacao do Node.js
 
@@ -50,7 +50,7 @@ for %%X in (node.exe) do (set HAS_NODE=%%~$PATH:X)
 if not defined HAS_NODE (
     echo Baixando o Node.js. Por favor, aguarde...
 
-    curl -O https://nodejs.org/dist/v16.14.0/node-v16.14.0-x64.msi --ssl-no-revoke
+    curl -m 600 -O https://nodejs.org/dist/v16.14.0/node-v16.14.0-x64.msi --ssl-no-revoke
 
     echo Iniciando a instalacao...
 
